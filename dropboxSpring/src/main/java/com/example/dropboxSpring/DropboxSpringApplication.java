c17d5e2cba8d79d00e2d5f3c2465f216a63d04fa
SimonLeBuono123 <simon.villoresi@gmail.com>
2023-11-23 09:47:59 +0100

    first commit spring init

---
+++ dropboxSpring/src/main/java/com/example/dropboxSpring/DropboxSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.dropboxSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropboxSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropboxSpringApplication.class, args);
+	}
+
+}
---
+++ dropboxSpring/src/test/java/com/example/dropboxSpring/DropboxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dropboxSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropboxSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
